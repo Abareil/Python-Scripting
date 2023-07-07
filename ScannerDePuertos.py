cb6ba678d1094b2948c008035291eff863096e2a
Abareil <ayelenbareilles@gmail.com>
2023-07-07 12:07:06 -0300

    Scanner dse puertos

---
+++ ScannerDePuertos.py
@@ -0,0 +1,20 @@
+
+#importar módulo socket
+import socket
+
+
+
+#ip de loopback para el ejemplo
+ip = '127.0.0.1' #puedo solicitar el puerto 
+
+for port in range(1,1000): #puedo definir el rango que necesite entre (1, 65535)
+    #inicio el socket para IPV4 y protocolo TCP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #defino el tiempo de espera
+    socket.setdefaulttimeout(1)
+    #conecto cliente con IP del servidor (el método connect_ex además devuelve error si no puede conectarse)
+    result = s.connect_ex((ip, port))
+    #imprimo puertos abiertos
+    if result==0:
+        print("Puerto {} abierto".format(port))
+    s.close()
